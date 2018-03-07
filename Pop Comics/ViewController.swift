ec8438b8f947f65e32f1a007a79e2b1bd65d1c41
Justin Oakes <admin@oklasoftware.com>
2018-03-06 21:32:45 -0600

    Initial Commit

---
+++ Pop Comics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pop Comics
+//
+//  Created by Justin Oakes on 6/3/18.
+//  Copyright © 2018 Oklasoft LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
