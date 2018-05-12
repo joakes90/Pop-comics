fa60c64464a645666c9b99dbd253c4e5f4854c58
justin <justin@justins-imac.attlocal.net>
2018-05-12 14:03:45 -0500

    added first time run alert enabled iTunes file sharing

--- Pop Comics/ViewController.swift
+++ Pop Comics/ViewController.swift
@@ -20,13 +20,28 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func test(_ sender: Any) {
-        let comicURL = Bundle.main.path(forResource: "Alias 002 (2001) (FBScan) - Unknown", ofType: "pdf")
-        let comicManager = ComicManager()
-        let book = comicManager.openComic(at: comicURL!)
-        testImageView.image = book?.last
-    }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        firstTimeRun()
+    }
+
+    private func firstTimeRun() {
+        if UserDefaults.standard.bool(forKey: "initialConfig") != true {
+            let alert = UIAlertController(title: NSLocalizedString("Enable iCloud", comment: "title for alert asking for iCloud permission"),
+                                          message: NSLocalizedString("Enabling iCloud makes importing your comics from your Mac or other iOS devices effortless", comment: "message for alert asking for iCloud permission"),
+                                          preferredStyle: .alert)
+            let sureButton = UIAlertAction(title: NSLocalizedString("Sure", comment: "sure"),
+                                           style: .default,
+                                           handler: nil)
+            let cancelButton = UIAlertAction(title: NSLocalizedString("Maybe later", comment: "maybe later"),
+                                             style: .cancel,
+                                             handler: nil)
+            alert.addAction(sureButton)
+            alert.addAction(cancelButton)
+            
+            present(alert, animated: true, completion: nil)
+        }
+    }
 }
 
