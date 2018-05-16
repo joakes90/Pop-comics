9754842ae98750e26f5323bbd827a7ccc6c1a5fd
Justin Oakes <admin@oklasoftware.com>
2018-05-15 21:13:52 -0500

    created detail view controller

---
+++ Pop Comics/Views/ComicCollectionViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  ComicCellCollectionViewCell.swift
+//  Pop Comics
+//
+//  Created by Justin Oakes on 15/5/18.
+//  Copyright © 2018 Oklasoft LLC. All rights reserved.
+//
+
+import UIKit
+
+class ComicCollectionViewCell: UICollectionViewCell {
+    
+}
+
+extension ComicCollectionViewCell: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+}
+
+extension ComicCollectionViewCell: UICollectionViewDelegate {
+    
+}
