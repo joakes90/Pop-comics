661b32960d8fd4a18e969d99337a9b5dbdd31fb8
Justin Oakes <admin@oklasoftware.com>
2018-06-05 20:15:34 -0500

    zooming implemented

--- Pop Comics/Views/BookPageViewController.swift
+++ Pop Comics/Views/BookPageViewController.swift
@@ -11,7 +11,12 @@ import UIKit
 class BookPageViewController: UIViewController {
 
     @IBOutlet weak var pageImageView: UIImageView!
-    @IBOutlet weak var toolBar: UIToolbar!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var scrollLeading: NSLayoutConstraint!
+    @IBOutlet weak var scrollTrailing: NSLayoutConstraint!
+    @IBOutlet weak var scrollTop: NSLayoutConstraint!
+    @IBOutlet weak var scrollBottom: NSLayoutConstraint!
+    
     
     var pageImage: UIImage?
     var delegate: BookViewDismissProtocol?
@@ -21,7 +26,21 @@ class BookPageViewController: UIViewController {
         super.viewDidLoad()
         pageImageView.image = pageImage
     }
+    
+    fileprivate func updateMinScaleForSize(_ size: CGSize) {
+        let widthScale = size.width / pageImageView.bounds.width
+        let heightScale = size.height / pageImageView.bounds.height
+        let minScale = min(widthScale, heightScale)
+        
+        scrollView.minimumZoomScale = minScale
+        scrollView.zoomScale = minScale
+        scrollView.maximumZoomScale = 5
+    }
 
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        updateMinScaleForSize(view.bounds.size)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -33,35 +52,47 @@ class BookPageViewController: UIViewController {
     }
 
     @IBAction func toggleToolbarVisable(_ sender: Any) {
-        if toolBar.isHidden {
-                showToolbar()
-        } else {
-            hideToolbar()
-        }
+//        if toolBar.isHidden {
+//                showToolbar()
+//        } else {
+//            hideToolbar()
+//        }
     }
     
     fileprivate func showToolbar() {
-        UIView.animate(withDuration: 0.25, animations: {
-            self.toolBar.isHidden = false
-            self.toolBar.layer.opacity = 1
-        }) { (complete) in
-            self.timer = Timer.scheduledTimer(timeInterval: 5.0,
-                                              target: self,
-                                              selector: #selector(self.hideToolbar),
-                                              userInfo: nil,
-                                              repeats: false)
-            
-        }
+//        UIView.animate(withDuration: 0.25, animations: {
+//            self.toolBar.isHidden = false
+//            self.toolBar.layer.opacity = 1
+//        }) { (complete) in
+//            self.timer = Timer.scheduledTimer(timeInterval: 5.0,
+//                                              target: self,
+//                                              selector: #selector(self.hideToolbar),
+//                                              userInfo: nil,
+//                                              repeats: false)
+//
+//        }
     }
     
     @objc fileprivate func hideToolbar() {
-        UIView.animate(withDuration: 0.5, animations: {
-            self.toolBar.layer.opacity = 0
-        }) { (complete) in
-            if complete {
-                self.toolBar.isHidden = true
-                self.timer?.invalidate()
-            }
-        }
+//        UIView.animate(withDuration: 0.5, animations: {
+//            self.toolBar.layer.opacity = 0
+//        }) { (complete) in
+//            if complete {
+//                self.toolBar.isHidden = true
+//                self.timer?.invalidate()
+//            }
+//        }
+    }
+}
+
+extension BookPageViewController: UIScrollViewDelegate {
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        print("zooming zooming zooming")
+        return pageImageView
+    }
+    
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        
     }
 }
