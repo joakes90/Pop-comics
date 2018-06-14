ed4b2b64bc39c3cf49ff5cf561d68bfe8dcba06e
Justin Oakes <admin@oklasoftware.com>
2018-06-13 21:03:53 -0500

    splitviewcontroller displays master view in porterage on iPad displays master on launch on iPhone

---
+++ Pop Comics/Views/SplitViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SplitViewController.swift
+//  Pop Comics
+//
+//  Created by Justin Oakes on 13/6/18.
+//  Copyright © 2018 Oklasoft LLC. All rights reserved.
+//
+
+import UIKit
+
+class SplitViewController: UISplitViewController,UISplitViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        preferredDisplayMode = .allVisible
+        delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+        return true
+    }
+
+}
